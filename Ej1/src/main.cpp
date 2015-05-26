70e20d93cc7489c73e8f466566b4020f7311b565
pablisho <rodriguez.pabloariel@gmail.com>
2015-05-25 23:56:56 -0300

    Version final Ej1 posta

--- Ej1/src/main.cpp
+++ Ej1/src/main.cpp
@@ -4,11 +4,22 @@
 #include <iostream>
 #include <unistd.h>
 #include <string.h>
+#include <stdlib.h>
 
 using namespace std;
 
 int main(int argc, char* argv[]){
-    Lanzador lanzador(5,10,1000000,6);
+    if(argc != 5){
+        char* ms = "Cant incorrecta de parametros. \n Params cantPuertas, personasXpuerta, usecs, capMaxima\n";
+        write(STDOUT_FILENO,ms, strlen(ms));
+        exit(1);
+    }
+    int cantPuertas = atoi(argv[1]);
+    int personas = atoi(argv[2]);
+    int usecs = atoi(argv[3]);
+    int capMaxima =  atoi(argv[4]);
+
+    Lanzador lanzador(cantPuertas,personas,usecs,capMaxima);
     lanzador.iniciarIPC();
     lanzador.lanzarPuertas();
 
